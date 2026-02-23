336decc12f5d5cced665527137bd2c74dcac9888
george-kajaia <54777047+george-kajaia@users.noreply.github.com>
2026-02-24 00:26:18 +0500

    Change Investor buttons

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   // Base URL for Company/Investor/Product/Request/User controllers
-  apiBaseUrl: 'https://ec2-3-66-156-73.eu-central-1.compute.amazonaws.com/api'
+  apiBaseUrl: 'https://service-tokens.com//api'
   //apiBaseUrl: 'https://ec2-3-66-156-73.eu-central-1.compute.amazonaws.com/servicetoken/api'
 };
